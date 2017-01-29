324241cfc5d7ca82c32e7167ce8a9a4684e84952
imears <maxwellmears@gmail.com>
2017-01-29 03:14:17 -0500

    Add event listener for clicking image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World From Script")
+// When the page is DOM (Document Object Model aka the page) is done loading run the code
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  document.getElementById('img').addEventListener('click', function(event) {
+      alert('clicked!');
+    }, false);
+
+});
